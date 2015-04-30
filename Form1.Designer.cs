faa681a57c8eb926bdd149dea63b44225656c54c
igor <igor>
2015-04-30 12:26:05 +0300

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VSPackageManager
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.Caption = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.CheckBoxes = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Caption,
+            this.Description});
+			this.listView1.Location = new System.Drawing.Point(12, 32);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(922, 482);
+			this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.ListViewItemChecked);
+			// 
+			// Caption
+			// 
+			this.Caption.Text = "Caption";
+			this.Caption.Width = 457;
+			// 
+			// Description
+			// 
+			this.Description.Text = "Description";
+			this.Description.Width = 547;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.button1.Location = new System.Drawing.Point(444, 520);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Refresh";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Filter:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(50, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(323, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(946, 555);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Package manager";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader Caption;
+		private System.Windows.Forms.ColumnHeader Description;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace VSPackageManager
+{
+	public partial class Form1 : Form
+	{
+		private static readonly string PackagesPath = @"Software\Microsoft\VisualStudio\12.0_Config\Packages";
+		List<ListViewItem> _items = new List<ListViewItem>();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			RefreshList();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			RefreshList();
+		}
+
+		bool _loading;
+		private void RefreshList()
+		{
+			_loading = true;
+
+			try
+			{
+				using (var packagesKey = Registry.CurrentUser.OpenSubKey(PackagesPath))
+				{
+					var subkeyNames = packagesKey.GetSubKeyNames();
+					foreach (var item in subkeyNames)
+					{
+						using (var packageKey = packagesKey.OpenSubKey(item))
+						{
+							var name = (string)packageKey.GetValue(null);
+							var desc = (string)packageKey.GetValue("ProductName");
+							var listViewItem = new ListViewItem
+							{
+								Text = name,
+								Checked = !item.StartsWith("!"),
+								Tag = item
+							};
+							listViewItem.SubItems.Add(new ListViewItem.ListViewSubItem { Text = desc });
+							_items.Add(listViewItem);
+						}
+					}
+				}
+				Filter();
+			}
+			finally
+			{
+				_loading = false;
+			}
+		}
+
+		private void ListViewItemChecked(object sender, ItemCheckedEventArgs e)
+		{
+			if (_loading)
+				return;
+
+			string keyName = (string)e.Item.Tag;
+			string newKeyName = string.Empty;
+			if (e.Item.Checked)
+			{
+				newKeyName = keyName.Replace("!", string.Empty);
+			}
+			else
+			{
+				newKeyName = "!" + keyName;
+			}
+
+			using (var packagesKey = Registry.CurrentUser.OpenSubKey(PackagesPath, true))
+			{
+				RegistryUtilities.RenameSubKey(packagesKey, keyName, newKeyName);
+				e.Item.Tag = newKeyName;
+			}
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			Filter();
+		}
+
+		private void Filter()
+		{
+			_loading = true;
+			try
+			{
+			listView1.Items.Clear();
+			foreach (var item in _items)
+			{
+				if(
+					!string.IsNullOrWhiteSpace(textBox1.Text)
+					&&
+					!item.Text.ToLower().Contains(textBox1.Text.ToLower())
+					)
+					continue;
+
+				listView1.Items.Add(item);
+			}
+			}
+			finally
+			{
+				_loading = false;
+			}
+		}
+	}
+
+	public static class RegistryUtilities
+	{
+		public static bool RenameSubKey(RegistryKey parentKey, string subKeyName, string newSubKeyName)
+		{
+			CopyKey(parentKey, subKeyName, newSubKeyName);
+			parentKey.DeleteSubKeyTree(subKeyName);
+			return true;
+		}
+		public static bool CopyKey(RegistryKey parentKey, string keyNameToCopy, string newKeyName)
+		{
+			using (RegistryKey destinationKey = parentKey.CreateSubKey(newKeyName))
+			{
+				using (RegistryKey sourceKey = parentKey.OpenSubKey(keyNameToCopy))
+				{
+					RecurseCopyKey(sourceKey, destinationKey);
+				}
+			}
+			return true;
+		}
+
+		private static void RecurseCopyKey(RegistryKey sourceKey, RegistryKey destinationKey)
+		{
+			foreach (string valueName in sourceKey.GetValueNames())
+			{
+				object objValue = sourceKey.GetValue(valueName);
+				RegistryValueKind valKind = sourceKey.GetValueKind(valueName);
+				destinationKey.SetValue(valueName, objValue, valKind);
+			}
+
+			foreach (string sourceSubKeyName in sourceKey.GetSubKeyNames())
+			{
+				using (RegistryKey sourceSubKey = sourceKey.OpenSubKey(sourceSubKeyName))
+				{
+					using (RegistryKey destSubKey = destinationKey.CreateSubKey(sourceSubKeyName))
+					{
+						RecurseCopyKey(sourceSubKey, destSubKey);
+					}
+				}
+			}
+		}
+	}
+
+}
